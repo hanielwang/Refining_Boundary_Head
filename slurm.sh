66e86c776dcedc4b88f3982f713ae24a22845238
hywang <302747091@qq.com>
2022-10-25 17:30:12 +0100

    Update slurm.sh

--- slurm.sh
+++ slurm.sh
@@ -10,6 +10,6 @@
 
 module load languages/anaconda3/2021-3.8.8-cuda-11.1-pytorch
 
-time python ./train.py ./configs/epic_slowfast.yaml --output reproduce_eval_train-17.20  --gau_sigma 5.5 --sigma1 0.5 --sigma2 0.5 --verb_cls_weight 0.5 --noun_cls_weight 0.5 
+time python ./train.py ./configs/epic_slowfast.yaml --output reproduce  --gau_sigma 5.5 --sigma1 0.5 --sigma2 0.5 --verb_cls_weight 0.5 --noun_cls_weight 0.5 
 
-time python ./eval.py ./configs/epic_slowfast.yaml ./ckpt/epic_slowfast_reproduce_eval_train-17.20/name_of_best_model --gau_sigma 5.5
+time python ./eval.py ./configs/epic_slowfast.yaml ./ckpt/epic_slowfast_reproduce/name_of_the_best_model --gau_sigma 5.5
